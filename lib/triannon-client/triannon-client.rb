1913db50221e31cac036e9898473a9093f7066e8
Darren L. Weber, Ph.D <darren.weber@stanford.edu>
2015-03-31 22:49:43 -0700

    Refactor and complete rspecs

--- lib/triannon-client/triannon-client.rb
+++ lib/triannon-client/triannon-client.rb
@@ -3,6 +3,9 @@ module TriannonClient
 
   class TriannonClient
 
+    CONTENT_TYPE_IIIF = 'application/ld+json; profile="http://iiif.io/api/presentation/2/context.json"'
+    CONTENT_TYPE_OA   = 'application/ld+json; profile="http://www.w3.org/ns/oa-context-20130208.json"'
+
     @@config = nil
 
     attr_accessor :site
@@ -20,19 +23,25 @@ module TriannonClient
     end
 
     # Delete an annotation
-    # @param iri [String] HTTP URL for a triannon annotation
+    # @param id [String] an annotation ID
     # @response [true|false] true when successful
-    def delete_annotation(iri)
-      uri = RDF::URI.parse(iri)
-      response = @site[uri.path].delete
-      # HTTP DELETE response codes:
-      # A successful response SHOULD be 200 (OK) if the response includes an
-      # entity describing the status, 202 (Accepted) if the action has not yet
-      # been enacted, or 204 (No Content) if the action has been enacted but the
-      # response does not include an entity.
-      [200, 202, 204].include? response.code
+    def delete_annotation(id)
+      begin
+        response = @site["/annotations/#{id}"].delete
+        # HTTP DELETE response codes:
+        # A successful response SHOULD be 200 (OK) if the response includes an
+        # entity describing the status, 202 (Accepted) if the action has not yet
+        # been enacted, or 204 (No Content) if the action has been enacted but the
+        # response does not include an entity.
+        [200, 202, 204].include? response.code
+      rescue => e
+        binding.pry if @@config.debug
+        @@config.logger.error("Failed to DELETE annotation: #{id}: #{e.response.code}, #{e.message}")
+        false
+      end
     end
 
+    # POST and open annotation to triannon; the response contains an ID
     # @param oa [JSON-LD] a json-ld object with an open annotation context
     # @return response [RestClient::Response|nil]
     def post_annotation(oa)
@@ -44,65 +53,16 @@ module TriannonClient
       tries = 0
       begin
         tries += 1
-        response = @site["/annotations/"].post post_data, :content_type => :json
+        response = @site["/annotations/"].post post_data, :content_type => 'application/ld+json'
       rescue => e
         sleep 1*tries
         retry if tries < 3
+        response = e.response
         binding.pry if @@config.debug
-        @@config.logger.error("Failed to POST annotation: #{e.message}")
+        @@config.logger.error("Failed to POST annotation: #{response.code}: #{response.body}")
       end
       return response
     end
-
-    # Get a list of annotations
-    # @param content_type [String] HTTP accept header value for content type negotiation
-    # @response [RDF::Graph|RestClient::Response] RDF::Graph when content_type is not specified
-    def get_annotations(content_type=nil)
-      if content_type.nil?
-        get("#{@site.url}/annotations")
-      else
-        get("/annotations", content_type)
-      end
-    end
-
-    # Get an annotation (with a default annotation context)
-    # @param id [String] String representation of an annotation URI
-    # @param content_type [String] HTTP accept header value for content type negotiation
-    # @response [RDF::Graph|RestClient::Response] RDF::Graph when content_type is not specified
-    def get_annotation(id, content_type=nil)
-      if content_type.nil?
-        get("#{@site.url}/annotations/#{id}")
-      else
-        get("/annotations/#{id}", content_type)
-      end
-    end
-
-    # Get an annotation with a IIIF context
-    # @param id [String] String representation of an annotation URI
-    # @param content_type [String] HTTP accept header value for content type negotiation
-    # @response [RDF::Graph|RestClient::Response] RDF::Graph when content_type is not specified
-    def get_iiif_annotation(id, content_type=nil)
-      if content_type.nil?
-        get("#{@site.url}/annotations/iiif/#{id}")
-      else
-        get("/annotations/iiif/#{id}", content_type)
-      end
-    end
-
-    # Get an annotation with an open annotation context
-    # @param id [String] String representation of an annotation URI
-    # @param content_type [String] HTTP accept header value for content type negotiation
-    # @response [RDF::Graph|RestClient::Response] RDF::Graph when content_type is not specified
-    def get_oa_annotation(id, content_type=nil)
-      if content_type.nil?
-        get("#{@site.url}/annotations/oa/#{id}")
-      else
-        get("/annotations/oa/#{id}", content_type)
-      end
-    end
-
-
-    private
 
     # GET annotations and annotation
     #
@@ -110,28 +70,94 @@ module TriannonClient
     # format ** default: jsonld ** also supports turtle, rdfxml, html
     # ** see https://github.com/sul-dlss/triannon/blob/master/app/controllers/triannon/annotations_controller.rb #show method for mime formats accepted
     #
-    # JSON-LD context
-    #
-    # You can request IIIF or OA context for jsonld. You can use either of
-    # these methods (with the correct HTTP Accept header):
-    #
-    # GET: http://(host)/annotations/iiif/(anno_id)
-    # GET: http://(host)/annotations/(anno_id)?jsonld_context=iiif
-    #
-    # GET: http://(host)/annotations/oa/(anno_id)
-    # GET: http://(host)/annotations/(anno_id)?jsonld_context=oa
-    #
-    # Note that OA (Open Annotation) is the default context if none is specified.
+    # Although triannon may not support them all, RDF::Format.content_types includes:
+    # 'application/n-triples' =>  [RDF::NTriples::Format]
+    # 'text/plain'            =>  [RDF::NTriples::Format]
+    # 'application/n-quads'   =>  [RDF::NQuads::Format]
+    # 'text/x-nquads'         =>  [RDF::NQuads::Format]
+    # 'application/ld+json'   =>  [JSON::LD::Format]
+    # 'application/x-ld+json' =>  [JSON::LD::Format]
+    # 'application/rdf+json'  =>  [RDF::JSON::Format]
+    # 'text/html'             =>  [RDF::RDFa::Format, RDF::RDFa::Lite, RDF::RDFa::HTML]
+    # 'application/xhtml+xml' =>  [RDF::RDFa::XHTML]
+    # 'image/svg+xml'         =>  [RDF::RDFa::SVG]
+    # 'text/n3'               =>  [RDF::N3::Format, RDF::N3::Notation3]
+    # 'text/rdf+n3'           =>  [RDF::N3::Format]
+    # 'application/rdf+n3'    =>  [RDF::N3::Format]
+    # 'application/rdf+xml'   =>  [RDF::RDFXML::Format, RDF::RDFXML::RDFFormat]
+    # 'application/trig'      =>  [RDF::TriG::Format]
+    # 'application/x-trig'    =>  [RDF::TriG::Format]
+    # 'application/trix'      =>  [RDF::TriX::Format]
+    # 'text/turtle'           =>  [RDF::Turtle::Format, RDF::Turtle::TTL]
+    # 'text/rdf+turtle'       =>  [RDF::Turtle::Format]
+    # 'application/turtle'    =>  [RDF::Turtle::Format]
+    # 'application/x-turtle'  =>  [RDF::Turtle::Format]
 
-    def get(uri, content_type=nil)
-      if content_type.nil?
-        # try to get an RDF graph, let RDF.rb do content negotiation
-        RDF::Graph.load(uri)
-      else
-        # content_type options should include: :html, :xml, :rdf, :json
-        content_type = content_type.to_sym
-        @site[uri].get({:accept => content_type})
+    # Get annotations
+    # @param content_type [String] HTTP mime type (defaults to 'application/ld+json')
+    # @response [RDF::Graph] RDF::Graph of open annotations
+    def get_annotations(content_type='application/ld+json')
+
+      # TODO: triannon is responding with HTML, not json-ld
+      # see https://github.com/sul-dlss/triannon/issues/117
+
+      response = @site['/annotations'].get({:accept => content_type})
+      # TODO: switch yard for different response.code?
+      response2graph(response, content_type)
+    end
+
+    # Get an annotation (with a default annotation context)
+    # @param id [String] String representation of an annotation ID
+    # @param content_type [String] HTTP mime type (defaults to 'application/ld+json')
+    # @response [RDF::Graph|nil] RDF::Graph of the annotation
+    def get_annotation(id, content_type='application/ld+json')
+      uri = "/annotations/#{id}"
+      response = @site[uri].get({:accept => content_type})
+      # TODO: switch yard for different response.code?
+      response2graph(response, content_type)
+    end
+
+    # Get an annotation using a IIIF context
+    # @param id [String] String representation of an annotation ID
+    # @response [RDF::Graph|nil] RDF::Graph of the annotation
+    def get_iiif_annotation(id)
+      get_annotation(id, CONTENT_TYPE_IIIF)
+    end
+
+    # Get an annotation using an open annotation context
+    # @param id [String] String representation of an annotation ID
+    # @response [RDF::Graph|nil] RDF::Graph of the annotation
+    def get_oa_annotation(id)
+      get_annotation(id, CONTENT_TYPE_OA)
+    end
+
+    # Parse an open annotation response into an RDF::Graph
+    # @param data [String] An open annotation in some serialized format
+    # @param content_type [String] An HTTP accept type (defaults to 'rdf+xml')
+    # @response graph [RDF::Graph] An RDF::Graph instance
+    def response2graph(data, content_type='application/rdf+xml')
+      g = RDF::Graph.new
+      # TODO: will RDF::Format work with the 'profile' parameter?
+      format = RDF::Format.for(:content_type => content_type)
+      format.reader.new(data) do |reader|
+        reader.each_statement {|s| g << s }
       end
+      g
+    end
+
+    # query an annotation graph to extract a URI for the first open annotation
+    # @param graph [RDF::Graph] An RDF::Graph of an open annotation
+    # @response uri [RDF::URI|nil] A URI for an open annotation
+    def annotation_uri(graph)
+      q = [:s, RDF.type, RDF::Vocab::OA.Annotation]
+      graph.query(q).collect {|s| s.subject }.first
+    end
+
+    # extract an annotation ID from the URI
+    # @param uri [RDF::URI] An RDF::URI for an annotation
+    # @response id [String|nil] An ID for an annotation
+    def annotation_id(uri)
+      uri.path.split('/').last
     end
 
   end
--- spec/lib/triannon-client/triannon-client_spec.rb
+++ spec/lib/triannon-client/triannon-client_spec.rb
@@ -2,43 +2,141 @@ require 'spec_helper'
 
 # ::TriannonClient::TriannonClient class specs
 
-describe TriannonClient do
+describe TriannonClient, :vcr do
+# describe TriannonClient do
+
+  # Note: could use a 'let' pattern, but create_annotation can track data.
+  # let(:post_response)  { @tc.post_annotation(@oa_jsonld) }
+  # let(:response_graph) { @tc.response2graph(post_response) }
+  # let(:annotation_uri) { @tc.annotation_uri(response_graph) }
+  # let(:annotation_id)  { @tc.annotation_id(annotation_uri) }
+
+  def create_annotation
+    r = @tc.post_annotation(@oa_jsonld)
+    g = @tc.response2graph(r)
+    uri = @tc.annotation_uri(g)
+    id = @tc.annotation_id(uri)
+    {
+      response: r,
+      graph: g,
+      uri: uri,
+      id: id
+    }
+  end
+
+  def check_graph_has_statements(graph)
+    expect(graph).to be_instance_of RDF::Graph
+    expect(graph.empty?).to be_falsy
+    expect(graph.size > 2).to be_truthy
+  end
+
+  before :all do
+    Dotenv.load
+    @tc = TriannonClient::TriannonClient.new
+    @oa_jsonld = '{"@context":"http://iiif.io/api/presentation/2/context.json","@graph":[{"@id":"_:g70349699654640","@type":["dctypes:Text","cnt:ContentAsText"],"chars":"I love this!","format":"text/plain","language":"en"},{"@type":"oa:Annotation","motivation":"oa:commenting","on":"http://purl.stanford.edu/kq131cs7229","resource":"_:g70349699654640"}]}'
+  end
+
+  before :each do
+    @anno = create_annotation
+  end
+
+  after :each do
+    @tc.delete_annotation(@anno[:id])
+  end
 
   describe "#delete_annotation" do
-    it 'should DELETE an open annotation' do
-      #TODO
+    it 'should DELETE an open annotation that exists' do
+      expect( @tc.delete_annotation(@anno[:id]) ).to be_truthy
+    end
+    it 'should fail to DELETE an open annotation that does NOT exist' do
+      id = 'anno_does_not_exist'
+      expect( @tc.delete_annotation(id) ).to be_falsy
     end
   end
 
   describe "#get_annotations" do
-    it 'should get a list of open annotations' do
-      #TODO
+    it 'should get an array of open annotations' do
+      annos = @tc.get_annotations
+      expect(annos).to be_instance_of Array
+      expect(annos.length > 0).to be_truthy
     end
   end
 
   describe "#get_annotation" do
-    it 'should get an open annotation by URI' do
-      #TODO
+    context 'with no content_type' do
+      it 'raises an internal server error with a nil ID' do
+        expect{@tc.get_annotation(nil)}.to raise_exception(RestClient::InternalServerError)
+      end
+      it 'raises an internal server error with an empty string ID' do
+        expect{@tc.get_annotation('')}.to raise_exception(RestClient::InternalServerError)
+      end
+      it 'returns an RDF graph with a valid ID for an annotation on the server' do
+        graph = @tc.get_annotation(@anno[:id])
+        check_graph_has_statements(graph)
+      end
+      it 'returns an empty RDF graph with a valid ID for NO annotations on the server' do
+        id = SecureRandom.uuid
+        graph = @tc.get_annotation(id)
+        expect(graph).to be_instance_of RDF::Graph
+        expect(graph.empty?).to be_truthy
+      end
     end
   end
 
   describe "#get_iiif_annotation" do
-    it 'should get an open annotation by URI, with IIIF context' do
-      #TODO
+    # the mime type is fixed as 'ld+json' for this method
+    it 'should get an open annotation by ID, using a IIIF profile' do
+      graph = @tc.get_iiif_annotation(@anno[:id])
+      check_graph_has_statements(graph)
     end
   end
 
   describe "#get_oa_annotation" do
-    it 'should get an open annotation by URI, with OA context' do
-      #TODO
+    # the mime type is fixed as 'ld+json' for this method
+    it 'should get an open annotation by ID, using an OA profile' do
+      graph = @tc.get_oa_annotation(@anno[:id])
+      check_graph_has_statements(graph)
     end
   end
 
   describe "#post_annotation" do
     it 'should POST an open annotation' do
-      #TODO
+      expect{@tc.post_annotation(@oa_jsonld)}.not_to raise_exception
+    end
+    it 'returns a RestClient::Response object' do
+      # The response behaves primarily as a String, so it can not be tested
+      # as an instance of RestClient::Response, but it can be tested to respond
+      # to RestClient methods, i.e.
+      r = @anno[:response]
+      expect(r.respond_to? 'code').to be_truthy
+      expect(r.respond_to? 'body').to be_truthy
+      expect(r.respond_to? 'headers').to be_truthy
     end
   end
+
+  describe '#response2graph' do
+    it 'turns RestClient::Response into RDF::Graph' do
+      expect(@anno[:graph]).to be_instance_of RDF::Graph
+    end
+  end
+
+  describe "#annotation_uri" do
+    it "extracts an RDF::URI from an RDF::Graph of an annotation" do
+      expect(@anno[:uri]).to be_instance_of RDF::URI
+    end
+    it "uri string is of form (host)/annotations/(uuid)" do
+      expect(@anno[:uri] =~ /annotations/).to be_truthy
+    end
+  end
+
+  describe "#annotation_id" do
+    it "extracts a String ID from the RDF::URI of an annotation" do
+      expect(@anno[:id]).to be_instance_of String
+    end
+  end
+
+
+
 
 end
 
