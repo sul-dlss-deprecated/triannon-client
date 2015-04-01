141aeb25dfdb8737e56ebfb1cdb81d0cc85f8573
Darren L. Weber, Ph.D <darren.weber@stanford.edu>
2015-03-31 17:48:04 -0700

    Add webmock and vcr gems

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,17 @@ Coveralls.wear!
 require 'triannon-client'
 require 'rspec'
 RSpec.configure do |config|
-
 end
 
+require 'vcr'
+cassette_ttl = 28 * 24 * 60 * 60  # 28 days, in seconds
+VCR.configure do |c|
+  c.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
+  c.hook_into :webmock
+  c.allow_http_connections_when_no_cassette = true
+  c.default_cassette_options = {
+    :record => :new_episodes,
+    :re_record_interval => cassette_ttl
+  }
+  c.configure_rspec_metadata!
+end
