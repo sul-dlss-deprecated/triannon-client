037fe1f419158476de8aff11ad69e145967571ca
Darren L. Weber, Ph.D <darren.weber@stanford.edu>
2015-04-01 17:24:15 -0700

    Revise specs and vcr cassettes

--- spec/lib/triannon-client/triannon-client_spec.rb
+++ spec/lib/triannon-client/triannon-client_spec.rb
@@ -3,7 +3,6 @@ require 'spec_helper'
 # ::TriannonClient::TriannonClient class specs
 
 describe TriannonClient, :vcr do
-# describe TriannonClient do
 
   # Note: could use a 'let' pattern, but create_annotation can track data.
   # let(:post_response)  { @tc.post_annotation(@oa_jsonld) }
@@ -45,6 +44,17 @@ describe TriannonClient, :vcr do
   end
 
   describe "#delete_annotation" do
+    it "should handle a 204 response to DELETE request" do
+      skip ("mock this properly")
+      # back_end = double()
+      # response = double
+      # allow(response),to receive(:code).and_return(204)
+      # allow(back_end).to receive(:delete).and_return(response)
+
+      # allow_any_instance_of(RestClient).to receive(:delete)
+      # allow(@tc).to receive(@sitle).and_return(back_end)
+      # expect(@tc.delete_annotation).to be true
+    end
     it 'should DELETE an open annotation that exists' do
       expect( @tc.delete_annotation(@anno[:id]) ).to be_truthy
     end
@@ -52,29 +62,48 @@ describe TriannonClient, :vcr do
       id = 'anno_does_not_exist'
       expect( @tc.delete_annotation(id) ).to be_falsy
     end
+    it "logging" do
+      skip ("mock this properly")
+      # expect(@tc.send(:logger)).to receive(:error).with("could not delete id ")
+      # # mock mock mock
+      # @tc.delete_annotation(non-existen-id)  or return 404 or whatever
+    end
+    it "doesn't call the back end" do
+      skip ("mock this properly")
+      # allow(@tc).to receive(@rest_client).and_return(mock)
+      # expect(@rest_client).not_to receive(:delete)
+      # @tc.del
+    end
   end
 
   describe "#get_annotations" do
     it 'should get an array of open annotations' do
-      annos = @tc.get_annotations
-      expect(annos).to be_instance_of Array
-      expect(annos.length > 0).to be_truthy
+      skip ("pending changes to triannon")
+      # annos = @tc.get_annotations
+      # expect(annos).to be_instance_of Array
+      # expect(annos.length > 0).to be_truthy
     end
   end
 
   describe "#get_annotation" do
+    context 'with content_type' do
+      #TODO
+    end
     context 'with no content_type' do
-      it 'raises an internal server error with a nil ID' do
+      it 'raises an argument error with a nil ID' do
         expect{@tc.get_annotation(nil)}.to raise_error(ArgumentError)
       end
-      it 'raises an internal server error with an empty string ID' do
+      it 'raises an argument error with an integer ID' do
+        expect{@tc.get_annotation(0)}.to raise_error(ArgumentError)
+      end
+      it 'raises an argument error with an empty string ID' do
         expect{@tc.get_annotation('')}.to raise_error(ArgumentError)
       end
       it 'returns an RDF graph with a valid ID for an annotation on the server' do
         graph = @tc.get_annotation(@anno[:id])
         check_graph_has_statements(graph)
       end
-      it 'returns an empty RDF graph with a valid ID for NO annotations on the server' do
+      it 'returns an EMPTY RDF graph with a valid ID for NO annotation on the server' do
         id = SecureRandom.uuid
         graph = @tc.get_annotation(id)
         expect(graph).to be_instance_of RDF::Graph
@@ -88,6 +117,7 @@ describe TriannonClient, :vcr do
     it 'should get an open annotation by ID, using a IIIF profile' do
       graph = @tc.get_iiif_annotation(@anno[:id])
       check_graph_has_statements(graph)
+      #TODO check that @tc client sends a request with the right profile header
     end
   end
 
@@ -96,6 +126,7 @@ describe TriannonClient, :vcr do
     it 'should get an open annotation by ID, using an OA profile' do
       graph = @tc.get_oa_annotation(@anno[:id])
       check_graph_has_statements(graph)
+      #TODO check that @tc client sends a request with the right profile header
     end
   end
 
@@ -124,14 +155,17 @@ describe TriannonClient, :vcr do
     it "extracts an RDF::URI from an RDF::Graph of an annotation" do
       expect(@anno[:uri]).to be_instance_of RDF::URI
     end
-    it "uri string is of form (host)/annotations/(uuid)" do
-      expect(@anno[:uri] =~ /annotations/).to be_truthy
+    it "RDF::URI is a valid URI" do
+      expect(@anno[:uri] =~ /\A#{URI::regexp}\z/).to be_truthy
     end
   end
 
   describe "#annotation_id" do
     it "extracts a String ID from the RDF::URI of an annotation" do
       expect(@anno[:id]).to be_instance_of String
+    end
+    it "extracts a String ID that is not empty" do
+      expect(@anno[:id].empty?).to be_falsy
     end
   end
 
