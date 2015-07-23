c7ec46ec14e90c49e4d8faea2672a954677a5e54
Darren L. Weber, Ph.D <darren.weber@stanford.edu>
2015-07-23 12:10:01 -0700

    Config - add authentication parameters

--- lib/triannon-client/configuration.rb
+++ lib/triannon-client/configuration.rb
@@ -6,10 +6,19 @@ module TriannonClient
     attr_accessor :debug
     attr_accessor :logger
 
+    # Parameters for triannon server
     attr_accessor :host
     attr_accessor :user
     attr_accessor :pass
+
+    # Parameters for triannon container
     attr_accessor :container
+    attr_accessor :container_user
+    attr_accessor :container_workgroups
+
+    # Parameters for triannon client authentication
+    attr_accessor :client_id
+    attr_accessor :client_pass
 
     def initialize
       @debug = env_boolean('DEBUG')
@@ -17,8 +26,16 @@ module TriannonClient
       @host = ENV['TRIANNON_HOST'] || 'http://localhost:3000'
       @user = ENV['TRIANNON_USER'] || ''
       @pass = ENV['TRIANNON_PASS'] || ''
+
+      # Parameters for triannon client authentication
+      @client_id   = ENV['TRIANNON_CLIENT_ID'] || ''
+      @client_pass = ENV['TRIANNON_CLIENT_PASS'] || ''
+
+      # Parameters for triannon container
       @container = ENV['TRIANNON_CONTAINER'] || ''
       @container += '/' unless(@container.empty? || @container.end_with?('/'))
+      @container_user = ENV['TRIANNON_CONTAINER_USER'] || ''
+      @container_workgroups = ENV['TRIANNON_CONTAINER_WORKGROUPS'] || ''
 
       # logger
       log_file = ENV['TRIANNON_LOG_FILE'] || 'triannon_client.log'
--- spec/lib/triannon-client/configuration_spec.rb
+++ spec/lib/triannon-client/configuration_spec.rb
@@ -68,6 +68,38 @@ module TriannonClient
       end
     end
 
+    describe '#client_id' do
+      it 'default value is an empty string' do
+        ENV['TRIANNON_CLIENT_ID'] = nil
+        config = Configuration.new
+        expect(config.client_id).to be_empty
+      end
+    end
+
+    describe '#client_id=' do
+      it 'can set value' do
+        config = Configuration.new
+        config.client_id = 'fred'
+        expect(config.client_id).to eql('fred')
+      end
+    end
+
+    describe '#client_pass' do
+      it 'default value is an empty string' do
+        ENV['TRIANNON_CLIENT_ID'] = nil
+        config = Configuration.new
+        expect(config.client_pass).to be_empty
+      end
+    end
+
+    describe '#client_pass=' do
+      it 'can set value' do
+        config = Configuration.new
+        config.client_pass = 'secret'
+        expect(config.client_pass).to eql('secret')
+      end
+    end
+
     describe '#container' do
       it 'default value is an empty string' do
         ENV['TRIANNON_CONTAINER'] = nil
@@ -79,8 +111,40 @@ module TriannonClient
     describe '#container=' do
       it 'can set value' do
         config = Configuration.new
-        config.container = 'secret'
-        expect(config.container).to eql('secret')
+        config.container = 'foo'
+        expect(config.container).to eql('foo')
+      end
+    end
+
+    describe '#container_user' do
+      it 'default value is an empty string' do
+        ENV['TRIANNON_CONTAINER_USER'] = nil
+        config = Configuration.new
+        expect(config.container_user).to be_empty
+      end
+    end
+
+    describe '#container_user=' do
+      it 'can set value' do
+        config = Configuration.new
+        config.container_user = 'joe'
+        expect(config.container_user).to eql('joe')
+      end
+    end
+
+    describe '#container_workgroups' do
+      it 'default value is an empty string' do
+        ENV['TRIANNON_CONTAINER_WORKGROUPS'] = nil
+        config = Configuration.new
+        expect(config.container_workgroups).to be_empty
+      end
+    end
+
+    describe '#container_workgroups=' do
+      it 'can set value' do
+        config = Configuration.new
+        config.container_workgroups = 'wgA'
+        expect(config.container_workgroups).to eql('wgA')
       end
     end
 
